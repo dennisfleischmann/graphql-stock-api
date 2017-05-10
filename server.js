45dccaa9e3fcf33853b2a3360eae6a84b81551fa
Dennis Fleischmann <dennis.fleischmann@hotmail.com>
2017-05-10 03:22:11 +0200

    added cors

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const schema = require('./schema/schema');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/graphql', expressGraphQL({
   schema,
   graphiql: true
