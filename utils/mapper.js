2f6c135f147eb13f4cdc4d87d2775658f3277645
Dennis Fleischmann <dennis.fleischmann@hotmail.com>
2017-05-30 22:46:06 +0200

    Revert "fix exposeStockData"
    This reverts commit 28df96ed779ce3fdc56b870006b053d5f22fdcad.

--- utils/mapper.js
+++ utils/mapper.js
@@ -3,7 +3,7 @@ const exposeStockData = stock => ({
   Code: stock.code,
   Price: stock.dataset_data.data[0][4],
   Exchange: '?',
-  Diff: (stock.dataset_data.data[0][4] - stock.dataset_data.data[1][4]).toFixed(2),
+  Diff: (stock.dataset_data.data[1][4] - stock.dataset_data.data[0][4]).toFixed(2),
 });
 
 module.exports = {
